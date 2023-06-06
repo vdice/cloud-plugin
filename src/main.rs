ed1f2d600474ee5dd29d9df30dd10e1e2f0a0456
Radu Matei <radu.matei@fermyon.com>
2023-06-06 19:53:33 +0200

    Update deploy to use OCI as distribution mechanism
    Signed-off-by: Radu Matei <radu.matei@fermyon.com>

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,6 @@ use commands::{
     deploy::DeployCommand, login::LoginCommand, sqlite::SqliteCommand, variables::VariablesCommand,
 };
 use semver::BuildMetadata;
-use spin_bindle::PublishError;
-use std::path::Path;
 
 /// Returns build information, similar to: 0.1.0 (2be4034 2022-03-31).
 const VERSION: &str = concat!(
@@ -49,23 +47,6 @@ async fn main() -> Result<(), Error> {
         CloudCli::Login(cmd) => cmd.run().await,
         CloudCli::Variables(cmd) => cmd.run().await,
         CloudCli::Sqlite(cmd) => cmd.run().await,
-    }
-}
-
-pub(crate) fn push_all_failed_msg(path: &Path, server_url: &str) -> String {
-    format!(
-        "Failed to push bindle from '{}' to the server at '{}'",
-        path.display(),
-        server_url
-    )
-}
-
-pub(crate) fn wrap_prepare_bindle_error(err: PublishError) -> anyhow::Error {
-    match err {
-        PublishError::MissingBuildArtifact(_) => {
-            anyhow!("{}\n\nPlease try to run `spin build` first", err)
-        }
-        e => anyhow!(e),
     }
 }
 
